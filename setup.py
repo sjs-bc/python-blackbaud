cf88bcb1bf4cb3a781a5ec860719fdccffaf364b
Ömer Boratav <oboratav@sfu.ca>
2023-05-21 13:29:27 -0700

    license

--- setup.py
+++ setup.py
@@ -5,14 +5,21 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setuptools.setup(
     name="blackbaud",
-    version="0.0.1",
+    version="0.1.0",
     author="Ömer Boratav",
     author_email="oboratav@stjohns.bc.ca",
     description="A python module to make the SKY API easier to work with",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/sjs-bc/python-blackbaud",
+    license="AGPLv3",
     project_urls={"Bug Tracker": "https://github.com/sjs-bc/python-blackbaud/issues"},
     packages=["blackbaud"],
-    install_requires=["requests", "requests-oauthlib", "requests-cache[json,redis]", "limits", "lxml"],
+    install_requires=[
+        "requests",
+        "requests-oauthlib",
+        "requests-cache[json,redis]",
+        "limits",
+        "lxml",
+    ],
 )
